7ecdc1c49fbb37dc3dcdd8cf4cb7a3db35bfeca8
Masahide Nakamura <masa-n@cs.kobe-u.ac.jp>
2020-08-07 23:42:10 +0900

    コメントを足した

--- MeetingRoomReservation/meeting-room-reservation/src/main/java/jp/kobe_u/cs27/app/meetingroomreservation/configuration/WebSecurityConfig.java
+++ MeetingRoomReservation/meeting-room-reservation/src/main/java/jp/kobe_u/cs27/app/meetingroomreservation/configuration/WebSecurityConfig.java
@@ -14,34 +14,27 @@ import jp.kobe_u.cs27.app.meetingroomreservation.domain.service.UserService;
 @Configuration
 @EnableWebSecurity //(1) Spring Securityを使うための設定
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
     @Autowired
     private UserService userService;         //予約アプリのユーザサービス
-
     @Autowired
     private PasswordEncoder passwordEncoder; //アプリ共通のパスワードエンコーダ
-
     @Autowired
     private AdminConfigration adminConfig;   //管理者の設定
-
     @Override
     public void configure(WebSecurity web) throws Exception {
         //  (2) 全体に対するセキュリティ設定を行う
-        //  特定のパスへのアクセスを認証の象から外す
+        //  特定のパスへのアクセスを認証の対象から外す
         web.ignoring().antMatchers("/img/**", "/css/**", "/js/**");
     }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
          //  (3) URLごとに異なるセキュリティ設定を行う
-
         //認可の設定
         http.authorizeRequests()
         .antMatchers("/login").permitAll()             //ログインページは誰でも許可
         .antMatchers("/users/**").hasRole("ADMIN")     //ユーザ管理は管理者のみ許可
         .antMatchers("/rooms/**").hasRole("ADMIN")     //会議室管理は管理者のみ許可
         .anyRequest().authenticated();                 //それ以外は全て認証必要
-
         //ログインの設定
         http.formLogin()
             .loginPage("/login")                       // ログインページ
@@ -50,7 +43,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .passwordParameter("password")             // パスワードに該当するリクエストパラメタ
             .defaultSuccessUrl("/reservations", true)  // 成功時のページ (trueは以前どこにアクセスしてもここに遷移する設定)
             .failureUrl("/login?error");               // 失敗時のページ
-
         //ログアウトの設定
         http.logout()
             .logoutUrl("/logout")                      //ログアウトのURL
@@ -58,16 +50,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .deleteCookies("JSESSIONID")               //クッキー削除
             .invalidateHttpSession(true)               //セッション情報消去
             .permitAll();                              //ログアウトはいつでもアクセスできる
-
     }
-
     @Autowired
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         //  (4) 認証方法の実装の設定を行う
         auth.userDetailsService(userService)   //認証は予約サービスのUserServiceを使う
             .passwordEncoder(passwordEncoder); //パスワードはアプリ共通のものを使う
-
-        // ついでに管理者サービスをここで登録しておく
+        // ついでにここで管理者ユーザを登録しておく
         userService.registerAdmin(adminConfig.getUsername(), adminConfig.getPassword());
     }
 }
